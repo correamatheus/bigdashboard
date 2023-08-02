46df13553ad1ec8a091d665ec8f1500e5f336be4
correamatheus <43376756+correamatheus@users.noreply.github.com>
2023-08-02 15:33:59 -0300

    feat: adicao de task e logica nfor do painel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { PainelComponent } from './dashboard/components/painel/painel.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TaskComponent } from './dashboard/components/task/task.component';
+import { IndexComponent } from './dashboard/components/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PainelComponent
+    PainelComponent,
+    TaskComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dashboard/components/painel/painel.component.ts
+++ src/app/dashboard/components/painel/painel.component.ts
@@ -10,7 +10,28 @@ export class PainelComponent implements OnInit {
 
   faMagnifyingGlass = faMagnifyingGlass;
   faBookmark = faBookmark;
-
+  updateCardsInfo = [
+    {
+      date: '02/08/23',
+      title: 'Atualização Dashboard 1',
+      desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, necessitatibus ea? Doloribus blanditiis provident consectetur voluptatem eum in expedita animi libero aliquid, magnam illo veniam adipisci fuga, exercitationem, dolore quos.'
+    },
+    {
+      date: '02/08/23',
+      title: 'Atualização Dashboard 2',
+      desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, necessitatibus ea? Doloribus blanditiis provident consectetur voluptatem eum in expedita animi libero aliquid, magnam illo veniam adipisci fuga, exercitationem, dolore quos.'
+    },
+    {
+      date: '02/08/23',
+      title: 'Atualização Dashboard 3',
+      desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, necessitatibus ea? Doloribus blanditiis provident consectetur voluptatem eum in expedita animi libero aliquid, magnam illo veniam adipisci fuga, exercitationem, dolore quos.'
+    },
+    {
+      date: '02/08/23',
+      title: 'Atualização Dashboard 4',
+      desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, necessitatibus ea? Doloribus blanditiis provident consectetur voluptatem eum in expedita animi libero aliquid, magnam illo veniam adipisci fuga, exercitationem, dolore quos.'
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
